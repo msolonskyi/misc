a20364a1abc02cb07cb47408e96778f72a0953c9
msolonskyi <mykola.solonskyi@gmail.com>
2024-11-17 10:10:54 -0500

    Git global settings

---
+++ settings/git.settings.sh
@@ -0,0 +1,5 @@
+git config --global user.name "msolonskyi" 
+git config --global user.email "mykola.solonskyi@gmail.com" 
+git config --global core.autocrlf true 
+git config --global core.safecrlf true
+git config core.autocrlf true

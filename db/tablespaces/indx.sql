943122b81c817e69212e575f07834f2b224c9e72
msolonskyi <mykola.solonskyi@gmail.com>
2024-11-17 13:42:20 -0500

    Tablespaces

---
+++ db/tablespaces/indx.sql
@@ -0,0 +1,2 @@
+create bigfile tablespace indx datafile '/u01/app/oracle/oradata/XE/indx.dbf' size 256m
+    autoextend on next 10m maxsize unlimited extent management local uniform size 64k logging segment space management auto;
